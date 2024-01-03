aa0d5834d967d44c8830e6288b13cf4019e26d81
SamCode011 <aaron2000-2000@outlook.com>
2024-01-03 17:23:52 -0500

    manipulation todo.store.js

--- TodoApp/src/store/todo.store.js
+++ TodoApp/src/store/todo.store.js
@@ -20,6 +20,43 @@ const initStore = () => {
     console.log('initStore 🚀');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented')
+}
+/**
+ * 
+ * @param {String} description
+ * */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+/**
+ * 
+ * @param {String} todoId
+ * */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+const deleteTodo = () => {
+    throw new Error('Not implemented');
+}
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+ 
 export default{
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
