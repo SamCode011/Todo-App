089b41301771589b390316e791773e8427e1df28
SamCode011 <aaron2000-2000@outlook.com>
2024-01-03 22:04:54 -0500

    validation and optimization

--- TodoApp/src/todos/use-cases/create-todo-html.js
+++ TodoApp/src/todos/use-cases/create-todo-html.js
@@ -7,10 +7,20 @@ import { Todo } from '../models/todo.model';
 
 export const createTodoHTML = (todo) => {
     if( !todo) throw new Error('A TODO object is required');
-     const html = `<h1>${ todo.description}</h1>`
+     const html = `
+     <div class="view">
+         <input class="toggle" type="checkbox" ${ todo.done ? 'checked':''}>
+         <label>${ todo.description}</label>
+         <button class="destroy"></button>
+     </div>
+     <input class="edit" value="Create a TodoMVC template">
+ `;
 
      const liElement = document.createElement('li');
      liElement.innerHTML = html ;
+     liElement.setAttribute('data-id',todo.id);
+     if(todo.done)
+     liElement.classList.add('completed');
 
      return liElement ;
 
--- TodoApp/src/todos/use-cases/render-todos.js
+++ TodoApp/src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from "../models/todo.model";
 import { createTodoHTML } from "./";
 
+let element;
+
 /**
  * @param {String}elementId
  * @param {Todo} todos
@@ -8,8 +10,11 @@ import { createTodoHTML } from "./";
 
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    //TODO reference 
-    const element = document.querySelector(elementId);
+if (!element)
+element = document.querySelector( elementId);
+
+if (!element) throw new Error(`Element ${elementId}not found`);
+element.innerHtml = '';
 
     todos.forEach( todo => {
         element.append( createTodoHTML(todo));
